b66d45b498598ca854236e0bb863569a29e67efe
mikehe123 <yhe48@u.rochester.edu>
2021-08-23 18:40:02 -0400

    sucess migrate to quill ref hook!

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -1,49 +1,50 @@
 import React, { useEffect } from "react";
-import ReactQuill, { quill } from "react-quill";
-import "react-quill/dist/quill.bubble.css";
+import { useQuill } from "react-quilljs";
+import "quill/dist/quill.bubble.css";
 import { useRecoilState, useRecoilValue } from "recoil";
-import {
-  argumentListState,
-  argumentIdState,
-  tabState,
-} from "../../state/atoms";
-import useKey from "../KeyPress";
+import { argumentListState, tabState } from "../../state/atoms";
 import { EditorContainer, EditorWrap } from "./EditorElements";
-import bindings, { content } from "./keybindings";
+import bindings from "./keybindings";
 
+const theme = "bubble";
+const modules = {
+  toolbar: false,
+  keyboard: {
+    bindings: bindings,
+  },
+};
 const Editor = ({ itemKey }) => {
-  // const tabPresssed = useKey("tab")
-  const [value, setValue] = React.useState("");
+  const { quill, quillRef } = useQuill({ theme, modules });
   const tabStateValue = useRecoilValue(tabState);
   const [argument, setArgument] = useRecoilState(argumentListState(itemKey));
 
-  const handleChange = (e) => {
-    setValue(e);
-  };
+  useEffect(() => {
+    // if (quill) {
+    //   quill.on("text-change", (delta, oldDelta, source) => {
+    //     console.log("Text change!");
+    //     console.log(quill.getText()); // Get text only
+    //     console.log(quill.getContents()); // Get delta contents
+    //     console.log(quill.root.innerHTML); // Get innerHTML using quill
+    //     console.log(quillRef.current.firstChild.innerHTML); // Get innerHTML using quillRef
+    //   });
+    // }
+  }, [quill]);
 
   useEffect(() => {
-    setArgument({
-      content: value,
-    });
-    setValue("");
+    if (quill) {
+      console.log("==========VVV=========");
+      console.log(quill.getContents());
+      setArgument({
+        content: quill.getText(),
+      });
+      quill.setContents("");
+    }
   }, [tabStateValue]);
   return (
     <>
       <EditorContainer>
         <EditorWrap>
-          <ReactQuill
-            preserveWhitespace={false}
-            onChangeSelection={(selection) => console.log(selection)}
-            theme="bubble"
-            value={value}
-            onChange={handleChange}
-            modules={{
-              toolbar: false,
-              keyboard: {
-                bindings: bindings,
-              },
-            }}
-          />
+          <div ref={quillRef} />
         </EditorWrap>
       </EditorContainer>
     </>
